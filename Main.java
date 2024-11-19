8c2afbf43c786f5a740552a640f73e55e079fee6
aleksandrgeorgiev <geosasha.mail@gmail.com>
2024-11-19 15:41:24 +0300

    .

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int[][] points = new int[][]{{100, 300}, {200, 400}, {50, 200}, {150, 150}, {300, 100}, {100,100}};
-        float[][][] screen = new float[500][500][3];
-        Display display = new Display(400, 400);
-        for (int x = 0; x < 500; x++) {
-            for (int y = 0; y < 500; y++) {
+        float[][][] screen = new float[600][600][3];
+        Display display = new Display(600, 600);
+        for (int x = 0; x < screen.length; x++) {
+            for (int y = 0; y < screen[0].length; y++) {
                 float lastLength = 1000;
                 int outputI = 0;
                 for (int i = 0; i < points.length; i++) {
